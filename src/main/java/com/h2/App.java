9c781fb285bab29c8927b8fb65fddf68eeb3effa
xavier0530 <130240310+xavier0530@users.noreply.github.com>
2023-04-09 14:58:34 -0400

    Completed First Java APP

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -10,8 +10,9 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
-
-    public static int doubleTheNumber(int number) {
+ public static int doubleTheNumber(int number){
+     return 2 * number;
+ }
 
     private static int add(int[] numbers) {
         var sum = 0;
